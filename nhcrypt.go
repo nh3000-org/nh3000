584b8d4a8c64ca791ad2ab4274b08a5f81386f22
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-01 16:39:55 -0500

    Create nhcrypt.go

---
+++ nhcrypt.go
@@ -0,0 +1,51 @@
+package nhcrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+)
+
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05} // must be 16 bytes
+
+type crypt interface {
+	Encrypt(string, string)        // plaintext, secret - encrypted
+	Decrypt(string, string) string //encrypted, secret - enencrypted
+}
+
+func encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		data = []byte(s)
+		//return []byte(s)
+	}
+	return data
+}
+
+func Encrypt(text string, secret string) (string, error) {
+	block, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return encode(cipherText), nil
+}
+
+func Decrypt(text string, secret string) (string, error) {
+	block, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	cipherText := decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText), nil
+}
