ab85a859c5ac3d58938274ee6b1103a4b1ba5770
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-05 15:57:33 -0500

    Update nhhash.go

--- nhhash.go
+++ nhhash.go
@@ -11,6 +11,8 @@ var Hash string
 type hash interface {
 	LoadWithDefault(string) // file name
 	Save(string, string)    // file name, hash
+	HashAndSalt([]byte) string
+	ComparePasswords(string,[]byte) bool
 }
 
 // provide file name and password to hash
@@ -49,4 +51,32 @@ func Save(filename string, hash string) (string, bool) {
 	}
 	Hash = hash
 	return nil, false
+}
+func HashAndSalt(pwd []byte) string {
+
+	// Use GenerateFromPassword to hash & salt pwd
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
+}
+
+func ComparePasswords(hashedPwd string, plainPwd []byte) bool {
+	// Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
 }
