65d8276a1f4fe12b0ef70f1d376ab7195ff7ad25
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-05 15:45:03 -0500

    Update nhcrypt.go

--- nhcrypt.go
+++ nhcrypt.go
@@ -4,6 +4,9 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05} // must be 16 bytes
@@ -11,19 +14,8 @@ var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 0
 type crypt interface {
 	Encrypt(string, string)        // plaintext, secret - encrypted
 	Decrypt(string, string) string //encrypted, secret - enencrypted
-}
-
-func encode(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
-}
-
-func decode(s string) []byte {
-	data, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {
-		data = []byte(s)
-		//return []byte(s)
-	}
-	return data
+	HashAndSaltash([]byte) string
+	ComparePasswords(string, []byte) bool
 }
 
 func Encrypt(text string, secret string) (string, error) {
@@ -48,4 +40,45 @@ func Decrypt(text string, secret string) (string, error) {
 	plainText := make([]byte, len(cipherText))
 	cfb.XORKeyStream(plainText, cipherText)
 	return string(plainText), nil
+}
+
+func HashAndSalt(pwd []byte) string {
+
+	// Use GenerateFromPassword to hash & salt pwd
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
+}
+
+func ComparePasswords(hashedPwd string, plainPwd []byte) bool {
+	// Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		data = []byte(s)
+	}
+	return data
 }
