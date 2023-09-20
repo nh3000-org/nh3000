7556145ff49798992334715114a514ca94407192
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-20 18:03:50 -0500

    Update nhhash.go

--- nhhash/nhhash.go
+++ nhhash/nhhash.go
@@ -1,9 +1,14 @@
 package nhhash
 
 import (
+	"log"
+	"os"
+
 	"fyne.io/fyne/v2/storage"
 	"golang.org/x/crypto/bcrypt"
-	"nhlang"
+
+	"github.com/nh3000-org/nh3000/nhlang"
+	"github.com/nh3000-org/nh3000/nhpref"
 )
 
 var Hash string
@@ -12,45 +17,45 @@ type hash interface {
 	LoadWithDefault(string) // file name
 	Save(string, string)    // file name, hash
 	HashAndSalt([]byte) string
-	ComparePasswords(string,[]byte) bool
+	ComparePasswords(string, []byte) bool
 }
 
 // provide file name and password to hash
 func LoadWithDefault(filename string, password string) (string, bool) {
-	lwderr, _ := storage.Exists(DataStore(filename))
+	lwderr, _ := storage.Exists(nhpref.DataStore(filename))
 	if lwderr == true {
-		wrt, errwrite := storage.Writer(DataStore(filename))
+		wrt, errwrite := storage.Writer(nhpref.DataStore(filename))
 		_, err2 := wrt.Write([]byte(Hash))
 		pwh, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
-			return GetLangs("hash-err2"), true
+			return nhlang.GetLangs("hash-err2"), true
 		}
 		if errwrite != nil || err2 != nil {
-			return GetLangs("hash-err1"), true
+			return nhlang.GetLangs("hash-err1"), true
 		}
 		Hash = string(pwh)
-		return nil, false
+		return Hash, false
 	}
-	ph, errf := os.ReadFile(DataStore(filename).Path())
+	ph, errf := os.ReadFile(nhpref.DataStore(filename).Path())
 	if errf != nil {
-		return GetLangs("hash-err3"), true
+		return nhlang.GetLangs("hash-err3"), true
 	}
 	Hash = string(ph)
-	return nil, false
+	return Hash, false
 }
 func Save(filename string, hash string) (string, bool) {
-	errf := storage.Delete(DataStore(filename))
+	errf := storage.Delete(nhpref.DataStore(filename))
 
 	if errf != nil {
-		return GetLangs("hash-err3"), true
+		return nhlang.GetLangs("hash-err3"), true
 	}
-	wrt, errwrite := storage.Writer(DataStore(filename))
+	wrt, errwrite := storage.Writer(nhpref.DataStore(filename))
 	_, err2 := wrt.Write([]byte(hash))
 	if errwrite != nil || err2 != nil {
-		return GetLangs("hash-err2"), true
+		return nhlang.GetLangs("hash-err2"), true
 	}
 	Hash = hash
-	return nil, false
+	return Hash, false
 }
 func HashAndSalt(pwd []byte) string {
 
