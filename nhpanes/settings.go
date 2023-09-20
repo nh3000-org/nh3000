0efe6e68c8713bc68fcb3c532776c67b6b0b83c8
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-20 18:00:16 -0500

    Update settings.go

--- nhpanes/settings.go
+++ nhpanes/settings.go
@@ -4,54 +4,57 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/nh3000-org/nh3000/nhlang"
+	"github.com/nh3000-org/nh3000/nhpref"
+	"github.com/nh3000-org/nh3000/nhutil"
 )
 
 func settingsScreen(_ fyne.Window) fyne.CanvasObject {
-	MyJson("LOAD")
-	lalabel := widget.NewLabel(GetLangs("ss-la"))
+	//	MyJson("LOAD")
+	lalabel := widget.NewLabel(nhlang.GetLangs("ss-la"))
 	la := widget.NewRadioGroup([]string{"eng", "spa"}, func(string) {})
 	la.Horizontal = true
-	la.SetSelected(PreferedLanguage)
+	la.SetSelected(nhpref.PreferedLanguage)
 
-	pllabel := widget.NewLabel(GetLangs("ss-pl"))
+	pllabel := widget.NewLabel(nhlang.GetLangs("ss-pl"))
 	pl := widget.NewRadioGroup([]string{"6", "8", "12"}, func(string) {})
 	pl.Horizontal = true
-	pl.SetSelected(PasswordMinimumSize)
+	pl.SetSelected(nhpref.PasswordMinimumSize)
 
-	malabel := widget.NewLabel(GetLangs("ss-ma"))
+	malabel := widget.NewLabel(nhlang.GetLangs("ss-ma"))
 	ma := widget.NewRadioGroup([]string{"12h", "24h", "161h", "8372h"}, func(string) {})
 	ma.Horizontal = true
-	ma.SetSelected(Msgmaxage)
+	ma.SetSelected(nhpref.Msgmaxage)
 
-	mcletterlabel := widget.NewLabel(GetLangs("ss-mcletter"))
+	mcletterlabel := widget.NewLabel(nhlang.GetLangs("ss-mcletter"))
 	mcletter := widget.NewRadioGroup([]string{"Yes", "No"}, func(string) {})
 	mcletter.Horizontal = true
-	mcletter.SetSelected(PasswordMustContainLetter)
+	mcletter.SetSelected(nhpref.PasswordMustContainLetter)
 
-	mcnumberlabel := widget.NewLabel(GetLangs("ss-mcnumber"))
+	mcnumberlabel := widget.NewLabel(nhlang.GetLangs("ss-mcnumber"))
 	mcnumber := widget.NewRadioGroup([]string{"Yes", "No"}, func(string) {})
 	mcnumber.Horizontal = true
-	mcnumber.SetSelected(PasswordMustContainNumber)
+	mcnumber.SetSelected(nhpref.PasswordMustContainNumber)
 
-	mcspeciallabel := widget.NewLabel(GetLangs("ss-mcspecial"))
+	mcspeciallabel := widget.NewLabel(nhlang.GetLangs("ss-mcspecial"))
 	mcspecial := widget.NewRadioGroup([]string{"Yes", "No"}, func(string) {})
 	mcspecial.Horizontal = true
-	mcspecial.SetSelected(PasswordMustContainSpecial)
+	mcspecial.SetSelected(nhpref.PasswordMustContainSpecial)
 
-	ssbutton := widget.NewButton(GetLangs("ss-ss"), func() {
-		PreferedLanguage = la.Selected
-		Msgmaxage = ma.Selected
-		PasswordMustContainNumber = mcnumber.Selected
-		PasswordMinimumSize = pl.Selected
-		PasswordMustContainLetter = mcletter.Selected
-		PasswordMustContainSpecial = mcspecial.Selected
-		if PasswordValid {
-			MyJson("SAVE")
+	ssbutton := widget.NewButton(nhlang.GetLangs("ss-ss"), func() {
+		nhpref.PreferedLanguage = la.Selected
+		nhpref.Msgmaxage = ma.Selected
+		nhpref.PasswordMustContainNumber = mcnumber.Selected
+		nhpref.PasswordMinimumSize = pl.Selected
+		nhpref.PasswordMustContainLetter = mcletter.Selected
+		nhpref.PasswordMustContainSpecial = mcspecial.Selected
+		if nhpref.PasswordValid {
+			nhpref.Save()
 		}
 	})
 
 	return container.NewCenter(container.NewVBox(
-		widget.NewLabelWithStyle(GetLangs("ss-heading"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(nhlang.GetLangs("ss-heading"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		lalabel,
 		la,
 		malabel,
@@ -66,7 +69,7 @@ func settingsScreen(_ fyne.Window) fyne.CanvasObject {
 		mcspecial,
 		ssbutton,
 		container.NewHBox(
-			widget.NewHyperlink("newhorizons3000.org", parseURL("https://newhorizons3000.org/")),
+			widget.NewHyperlink("newhorizons3000.org", nhutil.ParseURL("https://newhorizons3000.org/")),
 		),
 		widget.NewLabel(""),
 	))
