c79a5c548d96aed4f820838c816987d9aa0b6051
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-20 17:56:54 -0500

    Update certificates.go

--- nhpanes/certificates.go
+++ nhpanes/certificates.go
@@ -4,73 +4,66 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/nh3000-org/nh3000/nhlang"
+	"github.com/nh3000-org/nh3000/nhpref"
+	"github.com/nh3000-org/nh3000/nhutil"
 )
 
 func certificatesScreen(_ fyne.Window) fyne.CanvasObject {
 
 	errors := widget.NewLabel("...")
-	if PasswordValid == true {
-		calabel := widget.NewLabel(GetLangs("cs-ca"))
-		ca := widget.NewMultiLineEntry()
-		ca.Resize(fyne.NewSize(320, 240))
-		ca.SetText(Caroot)
-
-		cclabel := widget.NewLabel(GetLangs("cs-cc"))
-		cc := widget.NewMultiLineEntry()
-		cc.SetText(Clientcert)
-
-		cklabel := widget.NewLabel(GetLangs("cs-ck"))
-		ck := widget.NewMultiLineEntry()
-		ck.SetText(Clientkey)
-
-		ssbutton := widget.NewButton(GetLangs("cs-ss"), func() {
-			errors.SetText("...")
-			if PasswordValid {
-				var iserrors = editEntry("CERTIFICATE", ca.Text)
-				if iserrors {
-					errors.SetText(GetLangs("cs-err1"))
-				}
-				iserrors = editEntry("CERTIFICATE", cc.Text)
-				if iserrors {
-					errors.SetText(GetLangs("cs-err2"))
-				}
-				iserrors = editEntry("KEY", ck.Text)
-				if iserrors {
-					errors.SetText(GetLangs("cs-err3"))
-				}
-				if !iserrors {
-					MyJson("SAVE")
-				}
-			}
-		})
-
-		return container.NewCenter(container.NewVBox(
-			widget.NewLabelWithStyle(GetLangs("cs-heading"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-			calabel,
-			ca,
-			cclabel,
-			cc,
-			cklabel,
-			cklabel,
-			ck,
-
-			ssbutton,
-			errors,
-			container.NewHBox(
-				widget.NewHyperlink("newhorizons3000.org", parseURL("https://newhorizons3000.org/")),
-			),
-			widget.NewLabel("_                                                                                             _"),
-		))
+	if nhpref.PasswordValid == false {
+		errors.SetText(nhlang.GetLangs("cs-lf"))
 	}
-	errors.SetText("Logon First")
-	return container.NewCenter(container.NewVBox(
-		widget.NewLabelWithStyle(GetLangs("cs-heading"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+	calabel := widget.NewLabel(nhlang.GetLangs("cs-ca"))
+	ca := widget.NewMultiLineEntry()
+	ca.Resize(fyne.NewSize(320, 240))
+	ca.SetText(nhpref.Caroot)
 
+	cclabel := widget.NewLabel(nhlang.GetLangs("cs-cc"))
+	cc := widget.NewMultiLineEntry()
+	cc.SetText(nhpref.Clientcert)
+
+	cklabel := widget.NewLabel(nhlang.GetLangs("cs-ck"))
+	ck := widget.NewMultiLineEntry()
+	ck.SetText(nhpref.Clientkey)
+
+	ssbutton := widget.NewButton(nhlang.GetLangs("cs-ss"), func() {
+		errors.SetText("...")
+		if nhpref.PasswordValid {
+			var iserrors = nhpref.Edit("CERTIFICATE", ca.Text)
+			if iserrors {
+				errors.SetText(nhlang.GetLangs("cs-err1"))
+			}
+			iserrors = nhpref.Edit("CERTIFICATE", cc.Text)
+			if iserrors {
+				errors.SetText(nhlang.GetLangs("cs-err2"))
+			}
+			iserrors = nhpref.Edit("KEY", ck.Text)
+			if iserrors {
+				errors.SetText(nhlang.GetLangs("cs-err3"))
+			}
+			if !iserrors {
+				nhpref.Save()
+			}
+		}
+	})
+
+	return container.NewCenter(container.NewVBox(
+		widget.NewLabelWithStyle(nhlang.GetLangs("cs-heading"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		calabel,
+		ca,
+		cclabel,
+		cc,
+		cklabel,
+		cklabel,
+		ck,
+
+		ssbutton,
 		errors,
 		container.NewHBox(
-			widget.NewHyperlink("newhorizons3000.org", parseURL("https://newhorizons3000.org/")),
-		),
-		widget.NewLabel(""),
-	))
+			widget.NewHyperlink("newhorizons3000.org", nhutil.ParseURL("https://newhorizons3000.org/")),
+			widget.NewLabel("_                                                                                             _"),
+		)))
 
 }
