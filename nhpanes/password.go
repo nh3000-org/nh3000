a3c30a161664d938f548ca8a4fdee8103b070269
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-09-20 17:59:47 -0500

    Update password.go

--- nhpanes/password.go
+++ nhpanes/password.go
@@ -2,58 +2,59 @@ package panes
 
 import (
 	"log"
-	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/nh3000-org/nh3000/nhhash"
+	"github.com/nh3000-org/nh3000/nhlang"
+	"github.com/nh3000-org/nh3000/nhpref"
+	"github.com/nh3000-org/nh3000/nhutil"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func passwordScreen(_ fyne.Window) fyne.CanvasObject {
 
 	password := widget.NewPasswordEntry()
-	password.SetPlaceHolder(GetLangs("ps-password"))
+	password.SetPlaceHolder(nhlang.GetLangs("ps-password"))
 
 	passwordc1 := widget.NewPasswordEntry()
-	passwordc1.SetPlaceHolder(GetLangs("ps-passwordc1"))
+	passwordc1.SetPlaceHolder(nhlang.GetLangs("ps-passwordc1"))
 	passwordc1.Disable()
 
 	passwordc2 := widget.NewPasswordEntry()
-	passwordc2.SetPlaceHolder(GetLangs("ps-passwordc2"))
+	passwordc2.SetPlaceHolder(nhlang.GetLangs("ps-passwordc2"))
 	passwordc2.Disable()
 	errors := widget.NewLabel("...")
 	// try the password
-	tpbutton := widget.NewButton(GetLangs("ps-trypassword"), func() {
+	tpbutton := widget.NewButton(nhlang.GetLangs("ps-trypassword"), func() {
 		var iserrors = false
-		Password = password.Text
-		pwh, err := bcrypt.GenerateFromPassword([]byte(Password), bcrypt.DefaultCost)
-		Passwordhash = string(pwh)
+		nhpref.Password = password.Text
+		pwh, err := bcrypt.GenerateFromPassword([]byte(nhpref.Password), bcrypt.DefaultCost)
+		nhpref.Passwordhash = string(pwh)
 		if err != nil {
 			iserrors = true
-			errors.SetText(GetLangs("ps-err1"))
-		}
-		_, confighasherr := os.Stat(DataStore("config.hash").Path())
-		if confighasherr != nil {
-
-			if MyHash("CREATE") {
-				errors.SetText(GetLangs("ps-err2"))
-			}
+			errors.SetText(nhlang.GetLangs("ps-err1"))
 		}
 
-		Password = password.Text
-		if MyHash("LOAD") {
-			errors.SetText(GetLangs("ps-err3"))
+		myhash, err1 := nhhash.LoadWithDefault("config.hash", "123456")
+		nhpref.Passwordhash = myhash
+		if err1 {
+			errors.SetText(nhlang.GetLangs("ps-err2"))
 		}
+
+		nhpref.Password = password.Text
+
 		// Comparing the password with the hash
-		if err := bcrypt.CompareHashAndPassword([]byte(Passwordhash), []byte(Password)); err != nil {
+		if err := bcrypt.CompareHashAndPassword([]byte(nhpref.Passwordhash), []byte(password.Text)); err != nil {
 
 			iserrors = true
-			errors.SetText(GetLangs("ps-err4"))
+			errors.SetText(nhlang.GetLangs("ps-err4"))
 		}
 		if !iserrors {
-			MyJson("LOAD")
-			errors.SetText(GetLangs("ps-err5"))
+			nhpref.Load()
+
+			//errors.SetText(nhlang.GetLangs("ps-err5"))
 
 			password.Disable()
 			passwordc1.Enable()
@@ -62,69 +63,60 @@ func passwordScreen(_ fyne.Window) fyne.CanvasObject {
 		}
 	})
 
-	cpbutton := widget.NewButton(GetLangs("ps-chgpassword"), func() {
+	cpbutton := widget.NewButton(nhlang.GetLangs("ps-chgpassword"), func() {
 		var iserrors = false
 
-		if editEntry("STRING", passwordc1.Text) == true {
+		if nhpref.Edit("STRING", passwordc1.Text) == true {
 			iserrors = true
-			errors.SetText(GetLangs("ps-err6"))
+			errors.SetText(nhlang.GetLangs("ps-err6"))
 		}
 
-		if editEntry("PASSWORD", passwordc1.Text) {
+		if nhpref.Edit("PASSWORD", passwordc1.Text) {
 			iserrors = true
-			errors.SetText(GetLangs("ps-err7"))
+			errors.SetText(nhlang.GetLangs("ps-err7"))
 		}
 		if passwordc1.Text != passwordc2.Text {
 			iserrors = true
-			errors.SetText(GetLangs("ps-err8"))
+			errors.SetText(nhlang.GetLangs("ps-err8"))
 		}
 		if !iserrors {
 			pwh, err := bcrypt.GenerateFromPassword([]byte(passwordc1.Text), bcrypt.DefaultCost)
-			Passwordhash = string(pwh)
+			nhpref.Passwordhash = string(pwh)
 
 			if err != nil {
+				errors.SetText(nhlang.GetLangs("ps-err9") + err.Error())
 				log.Fatal(err)
 			}
-
-			if MyHash("SAVE") {
-				errors.SetText(GetLangs("ps-err9"))
+			if !iserrors {
+				nhpref.Save()
 
 			}
 
 		}
-		Password = passwordc1.Text
-		if MyHash("LOAD") {
-			errors.SetText(GetLangs("ps-err10"))
+		nhpref.Password = passwordc1.Text
+		_, err := nhhash.LoadWithDefault("config.hash", "123456")
+		if err {
+			errors.SetText(nhlang.GetLangs("ps-err10"))
 			iserrors = true
-		}
-		// Comparing the password with the hash
-		err := bcrypt.CompareHashAndPassword([]byte(Passwordhash), []byte(passwordc1.Text))
-		if err != nil {
-			errors.SetText(GetLangs("ps-err11"))
-			iserrors = true
-		}
-		if !iserrors {
-			MyJson("SAVE")
-
 		}
 
 	})
 
 	return container.NewCenter(container.NewVBox(
-		widget.NewLabelWithStyle(GetLangs("ps-title1"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(nhlang.GetLangs("ps-title1"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		widget.NewLabelWithStyle("config.json", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-		widget.NewLabelWithStyle(GetLangs("ps-title2"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(nhlang.GetLangs("ps-title2"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 
 		password,
 		tpbutton,
-		widget.NewLabelWithStyle(GetLangs("ps-title3"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(nhlang.GetLangs("ps-title3"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		passwordc1,
 		passwordc2,
 		cpbutton,
 		errors,
 		container.NewHBox(
-			widget.NewHyperlink("newhorizons3000.org", parseURL("https://newhorizons3000.org/")),
-			widget.NewHyperlink("github.com", parseURL("https://github.com/nh3000-org/snats")),
+			widget.NewHyperlink("newhorizons3000.org", nhutil.ParseURL("https://newhorizons3000.org/")),
+			widget.NewHyperlink("github.com", nhutil.ParseURL("https://github.com/nh3000-org/snats")),
 		),
 		widget.NewLabel(""), // balance the header on the tutorial screen we leave blank on this content
 	))
