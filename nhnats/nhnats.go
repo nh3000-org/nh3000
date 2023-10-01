29ff7cd0ef2333119e27172eb7f2c8f8f34d8358
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-10-01 18:36:04 -0500

    Update nhnats.go

--- nhnats/nhnats.go
+++ nhnats/nhnats.go
@@ -1,8 +1,11 @@
 package nhnats
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
+
 	"log"
 	"net"
 	"os"
@@ -10,13 +13,15 @@ import (
 	"strings"
 	"time"
 
+	//"fyne.io/fyne/v2"
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 
-	//_ "nats.go"
 	"github.com/nh3000-org/nh3000/nhcrypt"
 	"github.com/nh3000-org/nh3000/nhlang"
+
 	"github.com/nh3000-org/nh3000/nhpref"
+	"github.com/nh3000-org/nh3000/nhutil"
 )
 
 type MessageStore struct {
@@ -30,7 +35,7 @@ type MessageStore struct {
 }
 
 var NatsMessages []MessageStore
-var MyMap = make(map[string]int)
+var MyAckMap = make(map[string]bool)
 
 func Send(m string) bool {
 	EncMessage := MessageStore{}
@@ -67,9 +72,9 @@ func Send(m string) bool {
 
 	EncMessage.MSalias = nhpref.Alias
 
-	EncMessage.MSnodeuuid = "\n" + nhlang.GetLangs("ms-ni") + nhpref.NodeUUID[0:8]
+	EncMessage.MSnodeuuid = "\n" + nhlang.GetLangs("ms-ni") + nhpref.NodeUUID
 	iduuid := uuid.New().String()
-	EncMessage.MSiduuid = "\n" + nhlang.GetLangs("ms-msg") + iduuid[0:8]
+	EncMessage.MSiduuid = "\n" + nhlang.GetLangs("ms-msg") + iduuid
 	EncMessage.MSdate = "\n" + nhlang.GetLangs("ms-on") + time.Now().Format(time.UnixDate)
 	EncMessage.MSmessage = m
 	//EncMessage += m
@@ -80,8 +85,28 @@ func Send(m string) bool {
 	ejson, _ := nhcrypt.Encrypt(string(jsonmsg), nhpref.Queuepassword)
 	//return []byte(ejson)
 
+	clientcert, err := tls.LoadX509KeyPair(nhpref.DataStore("cert.pem").Path(), nhpref.DataStore("key.pem").Path())
+	if err != nil {
+		log.Println("nhnats.go clientcert " + err.Error())
+	}
+
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	ok := rootCAs.AppendCertsFromPEM([]byte(nhpref.Caroot))
+	if !ok {
+		log.Println("nhnats.go rootCAs")
+	}
+	tlsConfig := &tls.Config{
+		RootCAs:      rootCAs,
+		Certificates: []tls.Certificate{clientcert},
+		//ClientAuth:   tls.RequireAndVerifyClientCert,
+	}
 	var errflag = false
-	nc, err := nats.Connect(nhpref.Server, nats.RootCAsMem([]byte(nhpref.Caroot)), nats.ClientCertMem([]byte(nhpref.Clientcert), []byte(nhpref.Clientkey)))
+	//nc, err := nats.Connect(nhpref.Server, nats.RootCAsMem([]byte(nhpref.Caroot)), nats.ClientCertMem([]byte(nhpref.Clientcert), []byte(nhpref.Clientkey)))
+	nc, err := nats.Connect(nhpref.Server, nats.Secure(tlsConfig))
 	if err != nil {
 		fmt.Println(nhlang.GetLangs("ls-err7") + err.Error())
 		errflag = true
@@ -100,11 +125,34 @@ func Send(m string) bool {
 	return errflag
 }
 func Receive() {
+	clientcert, err := tls.LoadX509KeyPair(nhpref.DataStore("cert.pem").Path(), nhpref.DataStore("key.pem").Path())
+	if err != nil {
+		log.Println("nhnats.go clientcert " + err.Error())
+	}
 
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	ok := rootCAs.AppendCertsFromPEM([]byte(nhpref.Caroot))
+	if !ok {
+		log.Println("nhnats.go rootCAs")
+	}
+	//m = make(MyAckMap[string]bool)
 	for {
 		NatsMessages = nil
 
-		nc, err := nats.Connect(nhpref.Server, nats.RootCAsMem([]byte(nhpref.Caroot)), nats.ClientCertMem([]byte(nhpref.Clientcert), []byte(nhpref.Clientkey)))
+		tlsConfig := &tls.Config{
+			RootCAs:      rootCAs,
+			Certificates: []tls.Certificate{clientcert},
+			//ClientAuth:   tls.RequireAndVerifyClientCert,
+		}
+
+		//nc, err := nats.Connect(nhpref.Server, nats.RootCAsMem([]byte(nhpref.Caroot)), nats.ClientCertMem([]byte(nhpref.Clientcert), []byte(nhpref.Clientkey)))
+		nc, err := nats.Connect(nhpref.Server, nats.Secure(tlsConfig))
+
+		//nc, err := nats.Connect(nhpref.Server, nats.RootCAsMem([]byte(nhpref.Caroot)), nats.ClientCertMem([]byte(nhpref.Clientcert), []byte(nhpref.Clientkey)))
 		if err != nil {
 			log.Println(nhlang.GetLangs("ms-err2"))
 
@@ -134,28 +182,36 @@ func Receive() {
 
 		msgs, errfetch := sub.Fetch(100)
 		if errfetch != nil {
-			log.Println(nhlang.GetLangs("ms-err5") + errfetch.Error())
+			log.Panic(nhlang.GetLangs("ms-err5") + errfetch.Error())
+
 			//log.Println("messages.go PullSubscribe Fetch ", errfetch)
 		}
-		if errfetch != nil {
-			log.Println(nhlang.GetLangs("ms-err5") + errfetch.Error())
 
-		}
-		log.Println(nhlang.GetLangs("ms-err6-1") + strconv.Itoa(len(msgs)) + GetLangs("ms-err6-2"))
+		//log.Println(nhlang.GetLangs("ms-err6-1") + strconv.Itoa(len(msgs)) + nhlang.GetLangs("ms-err6-2"))
 		if len(msgs) > 0 {
 			for i := 0; i < len(msgs); i++ {
-				msgs[i].Nak()
+
 				handleMessage(msgs[i])
+				//if AckMap(node) {
+				//	log.Println("acking " + node)
+				//	msgs[i].Ack()
+				//	delete(MyAckMap, node)
+				//} else {
+				msgs[i].Nak()
+				//}
 			}
 		}
 
+		if nhutil.GetMessageWin() != nil {
+			nhutil.GetMessageWin().SetTitle(nhlang.GetLangs("ms-err6-1") + strconv.Itoa(len(msgs)) + nhlang.GetLangs("ms-err6-2"))
+		}
 		time.Sleep(30 * time.Second)
 	}
 }
 
-func handleMessage(m *nats.Msg) {
+func handleMessage(m *nats.Msg) string {
 	ms := MessageStore{}
-	var inmap = true // unique message id
+	//var inmap = true // unique message id
 	ejson, err := nhcrypt.Decrypt(string(m.Data), nhpref.Queuepassword)
 	if err != nil {
 		ejson = string(m.Data)
@@ -165,17 +221,29 @@ func handleMessage(m *nats.Msg) {
 		ejson = nhlang.GetLangs("ms-unk")
 	}
 
-	inmap = nodeMap("MI" + ms.MSiduuid)
-	if inmap == false {
-		NatsMessages = append(NatsMessages, ms)
+	if nhpref.Filter {
+		if strings.Contains(ms.MSmessage, nhlang.GetLangs("ls-con")) || strings.Contains(ms.MSmessage, nhlang.GetLangs("ls-dis")) {
+			return ""
+		}
 	}
 
+	NatsMessages = append(NatsMessages, ms)
+	//	}
+
+	return ms.MSiduuid
 }
 
-func nodeMap(node string) bool {
-	_, x := MyMap[node]
+/* func AckMap(nodeuuid string) bool {
+	if nodeuuid == "" {
+		return false
+	}
+	_, x := MyAckMap[nodeuuid]
+	if x {
+		log.Println(nodeuuid)
+		log.Println(x)
+	}
 	return x
-}
+} */
 
 func Erase() {
 	log.Println(nhlang.GetLangs("ms-era"))
