b7689e2b087d1cf727e68fecf12ca6b6e3fb1df2
nh3000 <historicanimations@gmail.com>
2023-11-08 07:17:51 -0600

    File Enc/Dec

--- nhcrypt/nhcrypt.go
+++ nhcrypt/nhcrypt.go
@@ -3,10 +3,119 @@ package nhcrypt
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
+	"io"
+	"os"
 )
 
-var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05} // must be 16 bytes
+var keyAes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}  // must be 16 bytes
+var keyHmac = []byte{36, 45, 53, 21, 87, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05} // must be 16 bytes
+const BUFFER_SIZE int = 4096
+const IV_SIZE int = 16
+
+func EncryptFile(filePathIn, filePathOut string) error {
+	inFile, err := os.Open(filePathIn)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(filePathOut)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	iv := make([]byte, IV_SIZE)
+	_, err = rand.Read(iv)
+	if err != nil {
+		return err
+	}
+
+	aes, err := aes.NewCipher(keyAes)
+	if err != nil {
+		return err
+	}
+
+	ctr := cipher.NewCTR(aes, iv)
+	hmac := hmac.New(sha256.New, keyHmac)
+
+	buf := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := inFile.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		outBuf := make([]byte, n)
+		ctr.XORKeyStream(outBuf, buf[:n])
+		hmac.Write(outBuf)
+		outFile.Write(outBuf)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	outFile.Write(iv)
+	hmac.Write(iv)
+	outFile.Write(hmac.Sum(nil))
+
+	return nil
+}
+func DecryptFile(filePathIn, filePathOut string) error {
+	inFile, err := os.Open(filePathIn)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(filePathOut)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	iv := make([]byte, IV_SIZE)
+	_, err = rand.Read(iv)
+	if err != nil {
+		return err
+	}
+
+	aes, err := aes.NewCipher(keyAes)
+	if err != nil {
+		return err
+	}
+
+	ctr := cipher.NewCTR(aes, iv)
+	hmac := hmac.New(sha256.New, keyHmac)
+
+	buf := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := inFile.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		outBuf := make([]byte, n)
+		ctr.XORKeyStream(outBuf, buf[:n])
+		hmac.Write(outBuf)
+		outFile.Write(outBuf)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	outFile.Write(iv)
+	hmac.Write(iv)
+	outFile.Write(hmac.Sum(nil))
+
+	return nil
+}
 
 // encrypt string
 func Encrypt(text string, secret string) (string, error) {
@@ -15,7 +124,7 @@ func Encrypt(text string, secret string) (string, error) {
 		return "", err
 	}
 	plainText := []byte(text)
-	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cfb := cipher.NewCFBEncrypter(block, keyAes)
 	cipherText := make([]byte, len(plainText))
 	cfb.XORKeyStream(cipherText, plainText)
 	return encode(cipherText), nil
@@ -28,13 +137,12 @@ func Decrypt(text string, secret string) (string, error) {
 		return "", err
 	}
 	cipherText := decode(text)
-	cfb := cipher.NewCFBDecrypter(block, bytes)
+	cfb := cipher.NewCFBDecrypter(block, keyAes)
 	plainText := make([]byte, len(cipherText))
 	cfb.XORKeyStream(plainText, cipherText)
 
 	return string(plainText), nil
 }
-
 
 // encode to base64
 func encode(b []byte) string {
