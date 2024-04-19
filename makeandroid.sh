61b334a34d898fec8bf6032b17e21623311f453d
nh3000 <historicanimations@gmail.com>
2024-04-19 14:42:48 -0500

    V1

--- makeandroid.sh
+++ makeandroid.sh
@@ -2,6 +2,7 @@
 export ANDROID_SDK_HOME=/home/oem/Android/Sdk
 export ANDROID_NDK_HOME=/home/oem/Android/Sdk/ndk/25.2.9519653
 export GOPATH=/home/oem/go/bin
+cd /home/oem/go/src/github.com/nh3000-org/nh3000/cmd/gui
 keytool -genkey -v -keystore apkapk.keystore -alias apk -keyalg RSA -keysize 2048 -validity 10000
 /home/oem/go/bin/fyne package -os android -appID org.nh3000.nh3000 -icon ./logo.png
 
--- makelinux.sh
+++ makelinux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export GOPATH=/home/oem/go/bin
+cd /home/oem/go/src/github.com/nh3000-org/nh3000/cmd/gui
 echo "1"
 go build nhgui.go
 echo "2"
