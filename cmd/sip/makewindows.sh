8226d02aa52f9753489763bede8763162c31c37b
nh3000 <historicanimations@gmail.com>
2024-08-25 10:31:50 -0500

    Remove SIP

--- cmd/sip/makewindows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export ANDROID_SDK_HOME=/home/oem/Android/Sdk
-export ANDROID_NDK_HOME=/home/oem/Android/Sdk/ndk/25.2.9519653
-export GOPATH=/home/oem/go/bin
-/home/oem/go/bin/fyne-cross windows --arch amd64 -app-id org.nh3000.gui --debug  --icon ./Icon.png
-rm go.mod
-rm go.sum
-
